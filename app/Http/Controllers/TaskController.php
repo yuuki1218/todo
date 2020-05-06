99b3113ba40aee084b7b6ad27abbb85f0c4aaaad
yuuki1218 <1991sy1218@gmail.com>
2020-05-06 04:17:46 +0000

    task edit ok

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Folder;
 use App\Task;
 use App\Http\Requests\CreateTask;
+use App\Http\Requests\EditTask;
 
 class TaskController extends Controller
 {
@@ -43,6 +44,32 @@ class TaskController extends Controller
 
         return redirect()->route('tasks.index', [
         'id' => $current_folder->id,
+        ]);
+    }
+    
+    public function showEditForm(int $id, int $task_id)
+    {
+        $task = Task::find($task_id);
+
+        return view('tasks/edit', [
+        'task' => $task,
+        ]);
+    }
+    
+    public function edit(int $id, int $task_id, EditTask $request)
+    {
+    
+        $task = Task::find($task_id);
+
+    
+        $task->title = $request->title;
+        $task->status = $request->status;
+        $task->due_date = $request->due_date;
+        $task->save();
+
+    
+        return redirect()->route('tasks.index', [
+            'id' => $task->folder_id,
         ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,9 @@ Route::get('/', function () {
 Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
 Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
 Route::post('/folders/create', 'FolderController@create');
+
 Route::get('/folders/{id}/tasks/create', 'TaskController@showCreateForm')->name('tasks.create');
 Route::post('/folders/{id}/tasks/create', 'TaskController@create');
+
+Route::get('/folders/{id}/tasks/{task_id}/edit', 'TaskController@showEditForm')->name('tasks.edit');
+Route::post('/folders/{id}/tasks/{task_id}/edit', 'TaskController@edit');
